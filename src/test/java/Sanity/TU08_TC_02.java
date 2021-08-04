e887eb5f9b4d3eade29037f19eea9f3ee72090a3
abhilash <abhilash@splittytravel.com>
2021-08-04 15:58:16 +0530

    Added logger to balanced test class Added cross browser to testNG.xml file and bases class Run on Local working found ok Run on POM.xml working found ok

--- src/test/java/Sanity/TU08_TC_02.java
+++ src/test/java/Sanity/TU08_TC_02.java
@@ -14,18 +14,23 @@ public void HomPage() throws InterruptedException
 		logger8=event.createTest("Verifing the home page ");
 		POM_TU8_TC_05 p=new POM_TU8_TC_05(driver);
 		p.Destination();
+		log.info("Destination place is selected");
 		logger8.log(Status.PASS, "Destination place is selected");
 		Thread.sleep(1000);
 		p.Date();
-		logger8.log(Status.PASS, "Expected date selected");
+		log.info("Date is selected");
+		logger8.log(Status.PASS, "Date is selected");
 		Thread.sleep(1000);
 		p.Adult();
+		log.info("Room and Adults is selected");
 		logger8.log(Status.PASS, "Room and Adults is selected");
 		Thread.sleep(1000);
 		p.GO();
+		log.info("Click on go button");
 		logger8.log(Status.PASS, "Click on go button");
 		Thread.sleep(1000);
 		p.VerifySearchedLocation(SearchValid);
+		log.info("Verified the search page");
 		logger8.log(Status.PASS, "Verified the search page");
 		Thread.sleep(3000);
 }
